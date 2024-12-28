99981de7c139358b4df62b5e4aa7e22d55782c87
niharika1102 <niharikag1102@gmail.com>
2024-12-28 23:45:04 +0530

    Initial router setup
    React router initial setup done - router created and tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+//setting up router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home</h1>,
+  },
+  {
+    path: '/about',
+    element: <h1>about</h1>,
+  },
+]);
+
+//returning the specified route based on the url asked for
 const App = () => {
-  return <h1>Jobify</h1>;
+  return <RouterProvider router={router}/>;
 };
 
 export default App;
