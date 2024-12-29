b924c1181fccd611e84e8a4fc23f99bac803caf1
niharika1102 <niharikag1102@gmail.com>
2024-12-29 12:32:21 +0530

    Link Component
    Setup link component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Register from "./Register";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <h1>Login</h1>
+      <Link to="/register">Register here</Link>
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <>
+      <div>Register</div>
+      <Link to="/login">Login here</Link>
+    </>
+  );
+};
 
-export default Register
+export default Register;
