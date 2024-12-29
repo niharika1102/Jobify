6dd769fc726361dc0401e8e0baa1eee6184e139d
niharika1102 <niharikag1102@gmail.com>
2024-12-29 11:32:00 +0530

    Setup index.js
    Setup index.js and tested against router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomeLayout from "./pages/HomeLayout";
+import { HomeLayout } from "./pages"; //using named imports from index file
 
 //setting up router
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomeLayout/>,
+    path: "/",
+    element: <HomeLayout />,
   },
   {
-    path: '/about',
+    path: "/about",
     element: <h1>about</h1>,
   },
 ]);
 
 //returning the specified route based on the url asked for
 const App = () => {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
