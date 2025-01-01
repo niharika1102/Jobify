b15823b55390076f4d81fd3e8513ec9fbd688daf
niharika1102 <niharikag1102@gmail.com>
2025-01-01 22:43:14 +0530

    Landing page - I
    Created first iteration of landing page with styles

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,12 +1,32 @@
+// @ts-nocheck
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing</h1>
-      <div className="content">Hello user!! Welcome to jobify</div>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>tracking </span> app
+          </h1>
+          <p>
+            Empower Your Career Journey with Ease! Track your applications,
+            discover opportunities, and stay ahead in your job search—all in one
+            place. Your next big move starts here!
+          </p>
+          <Link to="/register" className="btn register-link">Register</Link>
+          <Link to="/login" className="btn">Login / Explore App</Link>
+        </div>
+        <img src={main} alt="landing-img" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
