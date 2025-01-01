4deeee4bd7bd6d9fe99be55f4c6fd32be9d7f679
niharika1102 <niharikag1102@gmail.com>
2025-01-01 11:27:07 +0530

    Learnt about Styled Components

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  font-size: 2.5rem;
+  color: blue;
+  background: pink;
+  border: 4px black solid;
+`;
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <>
+      <div>Landing</div>
+      <StyledBtn>Hello world!!</StyledBtn>
+    </>
+  );
+};
 
-export default Landing
+export default Landing;
