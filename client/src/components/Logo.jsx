f8d527ba3a080f8280673b1a92c0c83aae21ce52
niharika1102 <niharikag1102@gmail.com>
2025-01-01 23:38:14 +0530

    Logo component - II
    Added index.js for components directory

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 /* eslint-disable no-unused-vars */
 import React from "react";
-import logo from "../images/logo.svg";
+import logo from "../assets/images/logo.svg";
 
 const Logo = () => {
   return <img src={logo} alt="jobify" className="logo" />;
