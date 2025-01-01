7bf841e6c393355608638f53eb1da994476eb8f1
niharika1102 <niharikag1102@gmail.com>
2025-01-01 23:52:19 +0530

    Error page complete
    Styling and logic of error page complete

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,16 +1,31 @@
+// @ts-nocheck
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link, useRouteError } from "react-router-dom";
+import err from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   const e = useRouteError();
-  console.log(e);
 
+  if (e.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={err} alt="error" />
+          <h3>Oops! Page Not Found</h3>
+          <p>We can&apos;t seem to find the page you are looking for</p>
+          <Link to="/dashboard">Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <>
-      <h1>Error page</h1>
-      <Link to="/">Home --&gt;</Link>
-    </>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 
