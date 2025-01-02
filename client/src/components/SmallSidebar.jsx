b1f2928ad11c94586c4059124d873a5d4f4b9a35
niharika1102 <niharikag1102@gmail.com>
2025-01-02 22:16:59 +0530

    Small sidebar setup
    Setup small sidebar for smaller screen sizes

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
-    const data = useDashboardContext();
-    console.log(data);
-    
-  return <Wrapper>SmallSide bar</Wrapper>;
+  const data = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { path, text, icon } = link;
+              return (
+                <NavLink to={path} key={text} className="nav-link">
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
