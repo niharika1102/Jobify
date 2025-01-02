3423eca9ba7a91c5365c99512b96002b2508a119
niharika1102 <niharikag1102@gmail.com>
2025-01-02 11:40:12 +0530

    Login page complete
    Login page done with styles and form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 import { Link } from "react-router-dom";
-import Register from "./Register";
 
 const Login = () => {
   return (
-    <>
-      <h1>Login</h1>
-      <Link to="/register">Register here</Link>
-    </>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow
+          type="email"
+          name="email"
+          labelText="Email ID"
+          defaultValue="johndoe@gmail.com"
+        />
+        <FormRow
+          type="password"
+          name="password"
+          labelText="Password"
+          defaultValue="johndoe"
+        />
+        <button type="submit" className="btn btn-block">
+          Login
+        </button>
+        <button className="btn btn-block">Explore the App</button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
