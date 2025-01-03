ad495bb6f09c83ce8f922fbecb1646df8fbd5c14
niharika1102 <niharikag1102@gmail.com>
2025-01-03 23:09:00 +0530

    Express and nodemon setup
    Express and nodemon setup done and tested

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("server working!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello hooman");
+});
+
+app.listen(5100, () => {
+  console.log("Server listening on port 5100");
+});
