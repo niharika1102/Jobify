a4aae18d53632be20074227862c3d0e93ed10fcc
niharika1102 <niharikag1102@gmail.com>
2025-01-04 10:43:20 +0530

    Get all jobs route
    Get all jobs route created and tested on local dat

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ dotenv.config();
 
 import express from "express";
 import morgan from "morgan"; //provides logs of our requests. it is a middleware
+import { nanoid } from "nanoid";
+
+//local data for testing
+let jobs = [
+    { id: nanoid(), company: "cognida.ai", position: "frontend developer" },
+    { id: nanoid(), company: "microsoft", position: "product manager" }
+]
 
 const app = express();
 app.use(express.json()); //middleware setup
@@ -20,6 +27,11 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
+
+//get all jobs route
+app.get("/api/v1/jobs", (req, res) => {
+    res.status(200).json({jobs});       //adding a status is not complusory, but it is a good practice
+})
 
 const port = process.env.PORT || 5100;
 
