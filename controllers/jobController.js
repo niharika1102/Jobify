5fe99a06d48539d721369e3e60432cd620e96b8d
niharika1102 <niharikag1102@gmail.com>
2025-01-05 23:13:59 +0530

    Status codes updated
    Status codes updated with package

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import Job from "../models/jobModel.js";
+import { StatusCodes } from "http-status-codes";
 
 //local data for testing
 let jobs = [
@@ -10,13 +11,13 @@ let jobs = [
 //get all jobs controller
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({ jobs }); //adding a status is not complusory, but it is a good practice
+  res.status(StatusCodes.OK).json({ jobs }); //adding a status is not complusory, but it is a good practice
 };
 
 //create a job controller - we create the job in the schema using the 2nd line of the function
 export const createJob = async (req, res) => {
   const job = await Job.create(req.body); //req.body has all the values that we need to create a job like the company and the position. So, we can skip the destructing part
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 //get a job controller
@@ -24,9 +25,11 @@ export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
   if (!job) {
-    return res.status(404).json({ message: `No jobs found with id: ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: `No jobs found with id: ${id}` });
   }
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 //edit a job controller
@@ -37,9 +40,11 @@ export const updateJob = async (req, res) => {
     new: true,
   });
   if (!updatedJob) {
-    return res.status(404).json({ message: `No job found with ID ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: `No job found with ID ${id}` });
   }
-  res.status(200).json({ message: "Job updated", job: updatedJob });
+  res.status(StatusCodes.OK).json({ message: "Job updated", job: updatedJob });
 };
 
 //delete a job controller
@@ -47,7 +52,9 @@ export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndRemove(id);
   if (!removedJob) {
-    return res.status(404).json({ message: `No job found with id ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: `No job found with id ${id}` });
   }
-  return res.status(200).json({ message: "Job deleted" });
+  return res.status(StatusCodes.OK).json({ message: "Job deleted" });
 };
