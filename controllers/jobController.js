1e6f304411e5033d5d8926fed812d487ca015ed8
niharika1102 <niharikag1102@gmail.com>
2025-01-05 14:24:17 +0530

    All job controllers updated
    All job controllers updated as per database connections

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -9,6 +9,7 @@ let jobs = [
 
 //get all jobs controller
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs }); //adding a status is not complusory, but it is a good practice
 };
 
@@ -18,48 +19,35 @@ export const createJob = async (req, res) => {
   res.status(201).json({ job });
 };
 
-//get a job controller - We compare the id given by the user with the ones available in the database. If found, we return it. Else, we return error.
+//get a job controller
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-
+  const job = await Job.findById(id);
   if (!job) {
-    return res.status(404).json({ message: `no job found with id ${id}` });
+    return res.status(404).json({ message: `No jobs found with id: ${id}` });
   }
-
   res.status(200).json({ job });
 };
 
-//edit a job controller - We follow steps of create a job route and search a job. Additionally, we update the original company name and position with the one given to us.
+//edit a job controller
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-
-  if (!company || !position) {
-    return res.status(400).json({ message: "Enter company name and profile." });
-  }
-
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  //new: true returns the new job to us after updating. By default, the job returned is the one before the update.
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!updatedJob) {
     return res.status(404).json({ message: `No job found with ID ${id}` });
   }
-
-  job.company = company;
-  job.position = position;
-
-  res.status(200).json({ message: "Job updated", job });
+  res.status(200).json({ message: "Job updated", job: updatedJob });
 };
 
-//delete a job controller - We follow the steps of search for a single job. Additionally, we filter the objects from the jobs array and keep the ones whose id doesnt match with the one given to us. We store this in a variable. Then we update the original jobs array and return success.
+//delete a job controller
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-
-  if (!job) {
+  const removedJob = await Job.findByIdAndRemove(id);
+  if (!removedJob) {
     return res.status(404).json({ message: `No job found with id ${id}` });
   }
-
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
   return res.status(200).json({ message: "Job deleted" });
 };
