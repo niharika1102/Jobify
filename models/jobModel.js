e3ecb670001c1d721add62031511dd74316c1c10
niharika1102 <niharikag1102@gmail.com>
2025-01-05 12:41:22 +0530

    Update in job schema
    Job schema updated - don't consider previous commit

--- models/jobModel.js
+++ models/jobModel.js
@@ -2,27 +2,25 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const jobScehma = new Schema(
-  {
+const JobSchema = new Schema({
     company: String,
     position: String,
-    jobLocation: {
-      type: String,
-      default: "Hyderabad",
-    },
     jobStatus: {
-      //enum is used when the input can only be from a set of pre-defined values
       type: String,
-      enum: ["Pending", "Declined", "Interview Scheduled"],
-      default: "Pending",
+      enum: ['Interview', 'Declined', 'Pending'],
+      default: 'Pending',
     },
     jobType: {
       type: String,
-      enum: ["Internship", "Full Time", "Part Time"],
-      default: "Full Time",
+      enum: ['Full Time', 'Part Time', 'Internship'],
+      default: 'Full Time',
+    },
+    jobLocation: {
+      type: String,
+      default: 'My City',
     },
   },
-  { timestamps: true }
-);
+    { timestamps: true }
+)
 
-export default mongoose.model("Job", jobScehma);
+export default mongoose.model("Job", JobSchema);
