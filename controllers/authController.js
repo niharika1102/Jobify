f459eff09440fb9c685672c53e94bb97106bc31b
niharika1102 <niharikag1102@gmail.com>
2025-01-06 22:07:38 +0530

    Password hashing
    Password hashing logic added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0; //we count the number of users already created. If it is 0, i.e., this account is the first account, then we automatically set it admin. Else, it is user
   req.body.role = isFirstAccount ? "Admin" : "User";
+
+  //we generate a salt which is used to hash the passwords and then we take the password from the req.body and hash it and store it in req.body.
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({message: "User created successfully"});
 };
 
 export const login = async (req, res) => {
