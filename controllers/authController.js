605fcbeaaf645aa5dbc5d72acd97519648762e7f
niharika1102 <niharikag1102@gmail.com>
2025-01-06 21:53:58 +0530

    Admin functionality
    Admin privilege added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,12 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 
 export const register = async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user });
-}
+  const isFirstAccount = (await User.countDocuments()) === 0; //we count the number of users already created. If it is 0, i.e., this account is the first account, then we automatically set it admin. Else, it is user
+  req.body.role = isFirstAccount ? "Admin" : "User";
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
+};
 
 export const login = async (req, res) => {
-    res.send("Login");
-}
+  res.send("Login");
+};
