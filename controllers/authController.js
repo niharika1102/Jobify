311823434f9c80dfdedc8ae2d6f39609a08eefd4
niharika1102 <niharikag1102@gmail.com>
2025-01-06 22:11:45 +0530

    Util setup for hashing
    Util setup for hashing logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0; //we count the number of users already created. If it is 0, i.e., this account is the first account, then we automatically set it admin. Else, it is user
   req.body.role = isFirstAccount ? "Admin" : "User";
-
-  //we generate a salt which is used to hash the passwords and then we take the password from the req.body and hash it and store it in req.body.
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({message: "User created successfully"});
+  res
+    .status(StatusCodes.CREATED)
+    .json({ message: "User created successfully" });
 };
 
 export const login = async (req, res) => {
---
+++ utils/passwordUtils.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcryptjs";
+
+export async function hashPassword(password) {
+  //we generate a salt which is used to hash the passwords and then we take the password from the req.body and hash it and store it in req.body.
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
