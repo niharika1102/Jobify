62c173d8d7271951e197ea1fad2bede894d6f624
niharika1102 <niharikag1102@gmail.com>
2025-01-06 15:32:50 +0530

    Created user schema

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  lastName: {
+    type: String,
+    default: "LastName",
+  },
+  location: {
+    type: String,
+    default: "My City",
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin"],
+    default: "User",
+  },
+});
+
+export default mongoose.model('User', UserSchema)
