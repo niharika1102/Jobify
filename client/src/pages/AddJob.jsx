3ea903b5890e1d58957f0afdd7a3c3d50e93c834
niharika1102 <niharikag1102@gmail.com>
2025-01-09 10:57:41 +0530

    Add job form partially done
    Add job form functionality partially complete

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,8 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import { FormRow } from "../components";
 
 const AddJob = () => {
-  return <div>AddJob</div>;
+  // @ts-ignore
+  const { user } = useOutletContext();
+  const navigate = useNavigation();
+  const isSubmitting = navigate.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form className="form" method="post">
+        <h4 className="form-title">Add A Job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow type="text" name="jobLocation" labelText="job location" defaultValue={user.location}/>
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
