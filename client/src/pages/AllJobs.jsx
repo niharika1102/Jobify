35a8bd659184d4eb419c94473d99366e13fea927
niharika1102 <niharikag1102@gmail.com>
2025-01-09 22:58:34 +0530

    Buggy code - II (All jobs)
    Buggy code for all jobs component

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,24 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
 import React from "react";
-import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { JobsContainer, SearchContainer } from "../components";
+import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
-import { SearchContainer, JobsContainer } from "../components";
+import { useContext, createContext } from "react";
 
-export const loader = async ({ request }) => {
+export const loader = async () => {
   try {
     const { data } = await customFetch.get("/jobs");
-    return {
-      data,
-    };
+    return { data };
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
+    toast.error(error?.response?.data?.message);
     return error;
   }
 };
 
 const AllJobs = () => {
+  // @ts-ignore
   const { data } = useLoaderData();
   return (
     <>
