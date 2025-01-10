6aebd38695d262d79f737d21a4e76104a252728d
niharika1102 <niharikag1102@gmail.com>
2025-01-10 23:21:56 +0530

    Profile action setup
    Profile action setup complete and tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ import { action as loginAction } from "./pages/Login";
 import { action as addJobAction } from "./pages/AddJob";
 import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
+import { action as profileAction } from "./pages/Profile";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
@@ -83,6 +84,7 @@ const router = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
+            action: profileAction,
           },
           {
             path: "admin",
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { FormRow } from "../components";
@@ -7,7 +8,27 @@ import { useNavigation, Form } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+
+  const file = formData.get("avatar");
+  if (file.size > 500000) {
+    toast.error("Image size is too large");
+    return null;
+  }
+
+  try {
+    await customFetch.patch("/users/update-user", formData);
+    toast.success("Profile updated successfully");
+  } catch (error) {
+    toast.error(error.response.data.message);
+  }
+
+  return null;
+};
+
 const Profile = () => {
+  // @ts-ignore
   const { user } = useOutletContext();
   const { name, lastName, email, location } = user;
 
@@ -40,7 +61,11 @@ const Profile = () => {
           />
           <FormRow type="email" name="email" defaultValue={email} />
           <FormRow type="text" name="location" defaultValue={location} />
-          <button type="submit" className="btn btn-block form-btn" disabled={isSubmitting}>
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
             {isSubmitting ? "Submitting..." : "Submit"}
           </button>
         </div>
