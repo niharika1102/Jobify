b0ccaf6869ca0742383e90a8d3d65eb98571020c
niharika1102 <niharikag1102@gmail.com>
2025-01-10 22:46:38 +0530

    Admin page complete
    Admin page completed and tested

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -7,6 +7,7 @@ import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import { StatItem } from "../components";
 
 export const loader = async () => {
   try {
@@ -23,7 +24,20 @@ const Admin = () => {
 
   return (
     <Wrapper>
-      <h2>Admin page</h2>
+      <StatItem
+        title="current users"
+        count={users}
+        bcg="#b7e4c7"
+        color="#52b788"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        bcg="#e0e8f9"
+        color="#647acb"
+        icon={<BsCalendarCheckFill/>}
+      />
     </Wrapper>
   );
 };
