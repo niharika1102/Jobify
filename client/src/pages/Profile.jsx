af34a2760760edf5023f27343db2937f769ce169
niharika1102 <niharikag1102@gmail.com>
2025-01-10 23:12:46 +0530

    Profile page frontend done
    Profile page frontend completed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,52 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { useNavigation, Form } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
 
-export default Profile
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form" encType="multipart/form-data">
+        <h4 className="form-title">profile</h4>
+        <div className="form-center">
+          <div className="form-row">
+            <label htmlFor="image" className="form-label">
+              Select an image file (Max 0.5 MB)
+            </label>
+            <input
+              type="file"
+              name="avatar"
+              id="avatar"
+              className="form-input"
+              accept="image/*"
+            />
+          </div>
+          <FormRow type="text" name="name" defaultValue={name} />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="last name"
+            defaultValue={lastName}
+          />
+          <FormRow type="email" name="email" defaultValue={email} />
+          <FormRow type="text" name="location" defaultValue={location} />
+          <button type="submit" className="btn btn-block form-btn" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Profile;
