4f47f30249fa46c260445caaccb8b8df24d236a4
niharika1102 <niharikag1102@gmail.com>
2025-01-10 15:50:36 +0530

    Setup of JobsContainer done
    JobsContainer setup done and tested

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,8 +1,29 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import Wrapper from "../assets/wrappers/JobsContainer"
+import Job from './Job'
+import { useAllJobsContext } from '../pages/AllJobs'
 
 const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display. Start by adding one.</h2>
+      </Wrapper>
+    )
+  }
+
   return (
-    <div>JobsContainer</div>
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />
+        })}
+      </div>
+    </Wrapper>
   )
 }
 
