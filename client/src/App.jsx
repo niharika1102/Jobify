c987ec167b37d7d8bfdf2597029fb11065cd97cc
niharika1102 <niharikag1102@gmail.com>
2025-01-10 17:40:21 +0530

    Action and loader setup
    Action and loader setup in Edit Job

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,8 +19,11 @@ import {
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { action as addJobAction } from "./pages/AddJob";
+import { action as editJobAction } from "./pages/EditJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
+
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const checkDefaultTheme = () => {
@@ -86,6 +89,8 @@ const router = createBrowserRouter([
           {
             path: "edit-job/:id",
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,7 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData, useParams } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
+export const loader = async ({params}) => {
+  return null;
+}
+
+export const action = async () => {
+  return null;
+}
 
 const EditJob = () => {
+  const params = useParams();
   return (
     <div>EditJob</div>
   )
