b9e7da41fed4194622f7797e476faff86ec1fc04
niharika1102 <niharikag1102@gmail.com>
2025-01-10 17:49:49 +0530

    Loader setup and tested
    Loader setup and tested for edit jobs

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 import { FormRow, FormRowSelect } from "../components";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { useLoaderData, useParams } from "react-router-dom";
@@ -9,19 +9,26 @@ import { Form, useNavigation, redirect } from "react-router-dom";
 import { toast } from "react-toastify";
 import customFetch from "../utils/customFetch";
 
-export const loader = async ({params}) => {
-  return null;
-}
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error.response.data.message);
+    return redirect("/dashboard");
+  }
+};
 
 export const action = async () => {
   return null;
-}
+};
 
 const EditJob = () => {
   const params = useParams();
-  return (
-    <div>EditJob</div>
-  )
-}
+  console.log(params);
+  // @ts-ignore
+  const { job } = useLoaderData();
+  return <div>EditJob</div>;
+};
 
-export default EditJob
+export default EditJob;
