b81c08ffecd48a4412da27eb93ee0232ed5f5f90
niharika1102 <niharikag1102@gmail.com>
2025-01-11 17:31:53 +0530

    Charts container setup
    Charts container setup complete

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import AreaChart from "./AreaChart";
+import BarChart from "./BarChart";
+import Wrapper from "../assets/wrappers/ChartsContainer";
 
-const ChartsContainer = () => {
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
   return (
-    <div>ChartsContainer</div>
-  )
-}
+    <Wrapper>
+      <h4>Monthly applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
+};
 
-export default ChartsContainer
+export default ChartsContainer;
