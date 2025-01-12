385eca265c5a2e9af5ef5eeeb37793cab5d38c46
niharika1102 <niharikag1102@gmail.com>
2025-01-12 23:49:45 +0530

    Image upload on cloudinary fixed
    Image upload issue on cloudinary fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
-import cloudinary from 'cloudinary'
-import { promises as fs } from 'fs';
+import cloudinary from "cloudinary";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   //we match the id given in the request object (i.e., the one with which the user has logged in to the website) to the ones in the database and return if we find the relevant one
@@ -26,18 +26,21 @@ export const updateUser = async (req, res) => {
   const newUser = { ...req.body }; //to delete the password from the user object
   delete newUser.password;
 
-  if (req.file) {  //if we are able to upload the image successfully to cloudinary, we remove it from the local system by using logic in line2 of the if block
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+  if (req.file) {
+    //if we are able to upload the image successfully to cloudinary, we remove it from the local system by using logic in line2 of the if block
+    const file = formatImage(req.file);
+    // @ts-ignore
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
 
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser); //first parameter is the ID of the user to be updated and the second parameter is the values to be updated. They will be accessed from the req.body.
   //if the user already has an image uploaded and is trying to upload a new one, we remove the previous one from cloudinary
-  if (req.file && updatedUser.avatarPublicId) {  //the second parameter is used to check if the user is uploading the image for the first time. 
-    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId)
+  if (req.file && updatedUser.avatarPublicId) {
+    //the second parameter is used to check if the user is uploading the image for the first time.
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
   }
-  
+
   res.status(StatusCodes.OK).json({ message: "Updated user" });
 };
